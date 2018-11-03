17ae056d60a533642affb0872fdc3a7fe622b6e3
phulsechinmay <phulsechinmay@gmail.com>
2018-11-02 23:27:02 -0500

    Fixed all of this punching nonsense

---
+++ CSCE315Project2/Assets/Scripts/Ragdoll Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public bool punch = false;
+    public bool jump = false;
+
+	// Use this for initialization
+	void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (punch)
+        {
+            punch = false;
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(10000.0f, 0.0f));
+        }
+        if (jump)
+        {
+            jump = false;
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0.0f, 10000.0f));
+        }
+    }
+}
